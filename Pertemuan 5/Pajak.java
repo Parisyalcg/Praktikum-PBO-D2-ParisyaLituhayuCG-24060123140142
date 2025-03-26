f4efa8a8d9a24e6fcbb2c11dd0456e2f3b2ad0e8
Parisya Lituhayu C G_24060123140142 <162858778+Parisyalcg@users.noreply.github.com>
2025-03-26 21:21:23 +0700

    Add files via upload

---
+++ Pertemuan 5/Pajak.java
@@ -0,0 +1,11 @@
+/*
+ * File : Pajak.java
+ * Name : Parisya Lituhayu Chandrawati Gunawarman
+ * Date : Senin, 24 Maret 2025
+ * Desc : Implementation of Pajak class
+ */
+
+public interface Pajak {
+    double hitungPajak();
+}
+
